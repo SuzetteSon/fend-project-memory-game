49c1235403a305bb433ddb2254f638a4a1cf8c27
Suzette Sonnekus <suzetsonekis@gmail.com>
2018-03-29 16:15:39 +0200

    Functions, cardClicked & displaycardSymbol working, issues with checkLength

--- copy.js
+++ copy.js
@@ -1,11 +1,16 @@
-/* 
- * Restart button
- */
-// This function removes the classes .show .open and .match all cards only have class .card
-function restartGame (array) {
-	for (const element of array) {
-		element.classList.remove('open', 'show', 'match');
-	}
+
+function cardClicked () {
+	 for(var i=0; i < allCardsClosedArray.length; i++){ 
+		allCardsClosedArray[i].addEventListener('click', function () { 
+		//alert('hello'); 
+			openCards.push(i);
+			console.log('click');
+			console.log(openCards);
+			displayCardSymbol(i);
+			checkLength(openCards);
+		}, false); 
+
+	}	
+
 }
-restartGame(allCardsClosedArray);
-shuffle(allCardsClosedArray);
+cardClicked();
--- js/app.js
+++ js/app.js
@@ -44,19 +44,18 @@ function restartGame (array) {
 		element.classList.remove('open', 'show', 'match');
 
 	}
-	console.log(allCardsClosedArray);
+	//console.log(allCardsClosedArray);
 }
 //console.log(restartGame(allCardsClosedArray));
 //shuffle(allCardsClosedArray);
+restartGame(allCardsClosedArray);
+//document.querySelector('.restart').addEventListener('click', restartGame(allCardsClosedArray)); //werk nie
 
-document.querySelector('.fa-repeat').addEventListener('click', restartGame(allCardsClosedArray));
+//let card = document.getElementById('toets');
+//let secondCard = document.getElementById('toets2');
+let openCards = [];
+let matchedCards = [];
 
-/*
- * Event listener for the restart button
- */
-
-//const restartButton = document.querySelectorAll('.fa fa-repeat');
-//restartButton.addEventListener('click', restartGame(allCardsClosedArray));
 
 
 /*
@@ -68,38 +67,22 @@ document.querySelector('.fa-repeat').addEventListener('click', restartGame(allCa
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-/*
- * set up the event listener for a card. If a card is clicked:*/
+ */ 
 
 
-let card = document.getElementById('toets');
-let secondCard = document.getElementById('toets2');
-let openCards = [];
-let matchedCards = [];
 
- /*  - display the card's symbol (put this functionality in another function that you call from this one)*/
 
- function displayCardSymbol (currentcard) {
- 	// Display must add the .open class and the .show class to the list element. (The current card we clicked on) This function will take the card as an imput parameter, 
- 	//it will find / access it's class element and set it to class="card open show".
- 	//className
- 	currentcard.classList.add('show', 'open')
- 	 /*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)*/
- 	// try to add the current card to a list of open cards.
- 	
- 	openCards.push(currentcard)
- 	console.log(currentcard); //print die huidige card
- 	console.log(openCards); //print die lys met die huidige card by
- }
- //displayCardSymbol(card);
- //displayCardSymbol(secondCard);
+
+
+
+
  
 
 function cardsDoMatch (array) {
  	// Remove className open and show from each list item and add match. 
  	// remove from openCards array and add to matchedCards array
-			 	for (const card of array) {
+ 				console.log("A match!"); 
+				for (const card of array) {
 			 		card.classList.remove('show', 'open');
 			 		card.classList.add('match');
 			 		openCards = [] //reset opnCards weer na empty
@@ -111,42 +94,89 @@ function cardsDoMatch (array) {
 function cardsDoNotMatch (array) {
  	// Remove className open and show from each list item. 
  	// Remove from openCards list
-			 	for (const card of array) {
-			 		card.classList.remove('show', 'open');
-			 		openCards = [];
-			 	}
-			 }
+ 	console.log("Not a match");
+		for (const card of array) {
+			 card.classList.remove('show', 'open');
+			 openCards = [];
+		}
+}
 
  /*  - if the list already has another card, check to see if the two cards match
 	//check list openCards' length, if =2, 
 	//if 2, compare types/list element e.g. diamond, bolt. array[0] === [1] ? */
 function checkLength (array) {
-	if (array.length === 2) {
+	console.log(openCards.length);
+	if (array.length < 2) {
+		console.log('nog nie 2 nie');
+	} else if (array.length === 2) {
 		/*+ if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)*/
-		if (array[0].getAttribute("type") === (array[1]).getAttribute("type")) {
-			console.log("A match!"); 
+		if (array[0].outerHTML === array[1].outerHTML) {
 			cardsDoMatch(openCards);
 
 		} else {
 			/* + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)*/
-			console.log("Not a match");
 			cardsDoNotMatch(openCards);
 		}
+
 	}
 }
 //checkLength(openCards);
+
+ /*  - display the card's symbol (put this functionality in another function that you call from this one)*/
+
+ function displayCardSymbol (current) {
+ 	// Display must add the .open class and the .show class to the list element. (The current card we clicked on) This function will take the card as an imput parameter, 
+ 	//it will find / access it's class element and set it to class="card open show".
+ 	//className
+ 	current.classList.add('show', 'open')
+ 	 /*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)*/
+ 	// try to add the current card to a list of open cards.
+ 		openCards.push(current);
+ 		checkLength(openCards);
+ 	//console.log(current); //print die huidige card
+ 	//console.log(openCards); //print die lys met die huidige card by
+ }
+
+ //displayCardSymbol(card);
+ //displayCardSymbol(secondCard);
+
+ /* set up the event listener for a card. If a card is clicked:*/
+
+function cardClicked () {
+	 for(const i of allCardsClosedArray){ 
+		i.addEventListener('click', function () { 
+		//alert('hello'); 
+			console.log('click');
+			console.log(openCards);
+			displayCardSymbol(i);
+			
+		}, false); 
+
+	}	
+
+}
+cardClicked();
+
+
 
 /* 
  * Create move counter
  */	
 // each time a different card is clicked, counter +1
 
-document.querySelector('.card').addEventListener('click', function () {
+/*document.querySelector('.card').addEventListener('click', function () {
     var x = document.querySelector('.moves');
     document.querySelector('.moves').innerHTML = Number(x.innerHTML) + 1;
 });
+*/
 
 
+for(var i=0; i < allCardsClosedArray.length; i++){ 
+	allCardsClosedArray[i].addEventListener('click', function () { 
+		//alert('hello'); 
+		console.log('A card was clicked');
+	}, false); 
+}
 
  /*    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  	// The move counter must +1 every time a new card is clicked on
@@ -159,22 +189,18 @@ function allCardsMatch (array) {
 }
  /*/
 }
-}
+}/*
+
+
+
+
 
 /* 
  * Create a timer
  */
 
 
-var timer = new Timer();
-timer.start();
-timer.addEventListener('secondsUpdated', function (e) {
-    $('#timerClock').html(timer.getTimeValues().toString());
-});
 
-/* 
- * Create a timer
- *
 
 /*function addUp() {
     var x = document.querySelector('.moves');
