3d9cafd3c844ed6d1bc5b3c9d1d2ae8c105d942b
Suzette Sonnekus <suzetsonekis@gmail.com>
2018-04-02 16:46:46 +0200

    Game finished :)

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 // global variables
-
-let movesMade;
+let movesMade = document.querySelector('.moves');
 let star;
 
 //Create a list that holds all cards
@@ -14,19 +13,13 @@ let matchedCards = []
 /*
  * Timer functionality 
  */
-var secCounter = 0;
-var clock = 0; 
-var minCounter = 0;
 
-
-
-function clockDisplay() {
-
-}
+let secCounter = 0;
+let clock = 0;
+let minCounter = 0;
 
 function startTimer() {
-	console.log('timer starting');
-	clock= setInterval(elapsedTime,1000);
+	clock = setInterval(elapsedTime, 1000);
 }
 
 function stopTimer() {
@@ -36,57 +29,22 @@ function stopTimer() {
 function elapsedTime() {
 	secCounter++;
 	if (secCounter >= 60) {
-        secCounter = 0;
-        minCounter++;
-    }
-	//console.log(secCounter);
-	let secDisplay = '0'+secCounter;
-	let minDisplay = '0'+minCounter;
+		secCounter = 0;
+		minCounter++;
+	}
+	//to show the zero before seconds and minutes less than 10
+	let secDisplay = '0' + secCounter;
+	let minDisplay = '0' + minCounter;
 	document.getElementById('seconds').innerText = secDisplay.slice(-2);
 	document.getElementById('minutes').innerText = minDisplay.slice(-2);
 }
 
 function resetTimer() {
 	secCounter = 0;
+	minCounter = 0;
 	document.getElementById('seconds').innerText = '00';
-	document.getElementById('minutes').innerText = '00';	
+	document.getElementById('minutes').innerText = '00';
 }
-/*var h1 = document.getElementsByTagName('h1')[0],
-    start = document.getElementById('start'),
-    stop = document.getElementById('stop'),
-    clear = document.getElementById('clear'),
-    seconds = 0, minutes = 0, hours = 0,
-    t;
-
-function add() {
-    seconds++;
-    if (seconds >= 60) {
-        seconds = 0;
-        minutes++;
-        if (minutes >= 60) {
-            minutes = 0;
-            hours++;
-        }
-    }
-    
-    h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
-
-    timer();
-}
-function timer() {
-    t = setTimeout(add, 1000);
-}
-
-// Stop timer function
-function stopTimer() {
-    clearTimeout(t);
-}
-
-// Clear timer function
-function resetTimer() {
-    h1.textContent = "00:00:00";
-    seconds = 0; minutes = 0; hours = 0;
-}*/
 
 /*
  * mainGameLogic function 
@@ -94,24 +52,19 @@ function resetTimer() {
 
 
 function mainGameLogic() {
-	if (secCounter > 0) {
-		console.log('timer has already started');
-	} else {
-		console.log('timer started')
+	if (secCounter > 0) {} else {
 		startTimer();
 	}
-	console.log(currentcard);
-// True = check if card is valid to click on
+	// True = check if card is valid to click on
 	if (checkIfCardClickedOnIsValid() === true) {
-		console.log('valid');
 		//log click counter
 		logClickCounter()
 		//update the stars
 		updateStarCounter()
 		//add currentcard to openCards list
-		addClickedCardToOpenCardsList ()
+		addClickedCardToOpenCardsList()
 		//check if there are 2 cards in openCards List
-		if (checkIf2CardsInOpenCardsList () === true) {
+		if (checkIf2CardsInOpenCardsList() === true) {
 			//check if the two cards in the array matches
 			if (checkIfMatched() === true) {
 				//if match, display cards as matched
@@ -119,109 +72,81 @@ function mainGameLogic() {
 				//add the 2 cards from openCards to MatchedCardsList and remove from OpenCards
 				addCardsToMatchedCardsList();
 				//check if the game is won
-				checkWinningCondition ()
+				checkWinningCondition()
 			} else {
 				//if not a match, display as wrong
 				updateDisplayWrong();
-				//wag 2 seconds voor cards close
-				//TODO add delay
-				//setTimeout(updateDisplayClosed,1000);
-				//updateDisplayClosed();
 			}
 		} else {
 			//display the card as open
-			console.log('nog net 1 card');
 			updateDisplayOpen();
 		}
 	} else {
-		console.log('invalid')
-		// invalid move do nothing
+		// invalid move, do nothing
 	}
 }
 
 //function to check the winning condition
-function checkWinningCondition () {
+function checkWinningCondition() {
 	if (matchedCards.length === 16) {
-		console.log('wen!');
 		overlay();
 	} else {
-		console.log('nog nie wen nie');
 		return false;
 	}
 }
 
 //function to add cards that match from openCards to matchedCards array
-function addCardsToMatchedCardsList () {
+function addCardsToMatchedCardsList() {
 	for (const q of openCards) {
 		matchedCards.push(q);
-		console.log(matchedCards);
 		//empty the openCards List
 		openCards = [];
-		console.log(openCards);
 	}
-	
+
 }
 
 //function to check if the two cards are a match in openCards List
-function checkIfMatched () {
+function checkIfMatched() {
 	if ((openCards[0].innerHTML) === (openCards[1].innerHTML)) {
-		console.log('these cards match');
 		return true;
 	} else {
-		console.log('these cards do not match');
 		return false;
 	}
 }
 
 //functions to update the display of the card
 //open and show
-function updateDisplayOpen () {
+function updateDisplayOpen() {
 	for (const o of openCards) {
 		o.classList.add('show', 'open');
- 		console.log('cards open');
 	}
-} 
+}
 //display matched cards
-function updateDisplayMatch () {
+function updateDisplayMatch() {
 	for (const m of openCards) {
- 		m.classList.add('match');
- 		console.log('cards matched');		
- 	}
-} 
+		m.classList.add('match');
+	}
+}
 //display wrong cards
-function updateDisplayWrong () {
+function updateDisplayWrong() {
 	for (const w of openCards) {
 		w.classList.add('wrong', 'show');
- 	}
- 	console.log('cards wrong');
- 	setTimeout(function(){
- 		for (const r of openCards) {
- 			r.classList.remove('wrong', 'show', 'open');
- 		}
-	openCards = [];
-	},1100)
+	}
+	setTimeout(function () {
+		for (const r of openCards) {
+			r.classList.remove('wrong', 'show', 'open');
+		}
+		openCards = [];
+	}, 1100)
 }
-//display closed cards
-/*function updateDisplayClosed () {
-	for (const c of openCards) {
-		c.classList.remove('wrong', 'show', 'open');
-		c.classList.add('closed');
- 		console.log('card closed');
-	}	
-	//remove cards from openCards List
-	openCards = [];
-	console.log(openCards);
-}*/
-
 
 //function to add clicked card to clicked cards list
-function addClickedCardToOpenCardsList () {
+function addClickedCardToOpenCardsList() {
 	openCards.push(currentcard);
-	console.log(openCards);
 }
 
 //function to check if there are two cards in the openCards List
-function checkIf2CardsInOpenCardsList () {
+function checkIf2CardsInOpenCardsList() {
 	if (openCards.length === 2) {
 		return true;
 	} else {
@@ -231,24 +156,18 @@ function checkIf2CardsInOpenCardsList () {
 
 // create a function to update the star counter
 function updateStarCounter() {
-	// when moves counter reaches 10, one star is removed
-	if (Number(movesMade.innerText) === 10) {
+	// when moves counter reaches 20, one star is removed
+	if (Number(movesMade.innerText) === 20) {
 		let star = document.querySelector('#star-one').classList.add('none');
-		//console.log(star);
-		//console.log('1 star now not visible')
-	} 
-	// when moves counter reaches 19, two stars are removed
-	if (Number(movesMade.innerText) === 19) {
+	}
+	// when moves counter reaches 29, two stars are removed
+	if (Number(movesMade.innerText) === 29) {
 		let star = document.querySelector('#star-two').classList.add('none');
-		//console.log(star);
-		//console.log('2 stars now not visible')
-	} 
-	// when moves counter reaches 25, three stars are removed
-	if (Number(movesMade.innerText) === 25) {
+	}
+	// when moves counter reaches 35, three stars are removed
+	if (Number(movesMade.innerText) === 35) {
 		let star = document.querySelector('#star-three').classList.add('none');
-		//console.log(star);
-		//console.log('3 stars now not visible')
-	} 
+	}
 	//when moves counter is zero, all stars should be shown
 	if (Number(movesMade.innerText) === 0) {
 		document.querySelector('#star-one').classList.remove('none');
@@ -260,16 +179,13 @@ function updateStarCounter() {
 // create log click counter function
 
 function logClickCounter() {
-    movesMade = document.querySelector('.moves'); 
-    movesMade.innerText=Number(movesMade.innerText) + 1;
-    console.log(movesMade);
-    //;
+	movesMade.innerText = Number(movesMade.innerText) + 1;
 }
 
 // create function to check if the card clicked on is a valid one
 
 function checkIfCardClickedOnIsValid() {
-	if(currentcard.classList.contains("open")) {
+	if (currentcard.classList.contains("open")) {
 		//do not allow click, invalid move
 		return false;
 	} else if (currentcard.classList.contains("show")) {
@@ -283,37 +199,30 @@ function checkIfCardClickedOnIsValid() {
 		return true;
 	}
 }
- /* set up the event listener for a card. If a card is clicked:*/
 
-function setUpEventListenerForCards () {
+// event listener set-up
+
+function setUpEventListenerForCards() {
 	//shuffle as soon as js is run
 	shuffle(allCardsClosedArray);
-	for(const i of allCardsClosedArray){ 
-		i.addEventListener('click', function () { 
-			console.log('click');
-			console.log(i)
-/*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)*/
-			currentcard  = i;
-			mainGameLogic()
+	for (const i of allCardsClosedArray) {
+		i.addEventListener('click', function () {
+			currentcard = i;
+			mainGameLogic();
 			currentcard = 0;
-		}, false); 
-
-	}	
-
+		}, false);
+	}
 }
 // this sets up event listeners on the page
 setUpEventListenerForCards();
 
 
-
 //shuffle function
-function shuffle(){
+function shuffle() {
 	var ul = document.querySelector('.deck');
-	console.log(ul);
 	for (var i = ul.children.length; i >= 0; i--) {
-    	ul.appendChild(ul.children[Math.random() * i | 0]);
+		ul.appendChild(ul.children[Math.random() * i | 0]);
 	}
-	console.log(ul);
 }
 
 /* 
@@ -321,11 +230,10 @@ function shuffle(){
  */
 
 // This function removes the classes .show .open and .match all cards only have class .card
-function restartGame () {
+function restartGame() {
 	//display all cards in closed condition
 	for (const element of allCardsClosedArray) {
 		element.classList.remove('open', 'show', 'match', 'wrong');
-		console.log('all cards now closed');
 	}
 	//empty the openCards array
 	openCards = []
@@ -344,29 +252,27 @@ function overlay() {
 	el = document.getElementById("overlay");
 	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
 	mm = document.getElementById("modal-message");
-	mm.innerText = ("Congradulations, you won! You have a rating of " 
-		+ howManyStars() + ". Your time is " +minCounter+ ":" +secCounter+".");
+	mm.innerText = ("Congradulations, you won! You have a rating of " +
+		howManyStars() + ". Your time is " + minCounter + ":" + secCounter + ".");
 }
 
-function closeOverlay () {
+function closeOverlay() {
 	el = document.getElementById("overlay");
 	el.style.visibility = "hidden";
 }
 
 //variable for amount of stars left.
-function howManyStars () {
-	if (Number(movesMade.innerText) < 10) {
-		return "3 stars"
-	} 
-	if (Number(movesMade.innerText) >9 && Number(movesMade.innerText) < 19 ) {
-		return "2 stars"
-	} 
-	if (Number(movesMade.innerText) >18  && Number(movesMade.innerText) < 26) {
-		return "1 star"
-	} 
-	if (Number(movesMade.innerText) >25) {
-		return "0 star"
-	} 
+function howManyStars() {
+	if (Number(movesMade.innerText) < 20) {
+		return "3 stars";
+	}
+	if (Number(movesMade.innerText) > 19 && Number(movesMade.innerText) < 29) {
+		return "2 stars";
+	}
+	if (Number(movesMade.innerText) > 28 && Number(movesMade.innerText) < 36) {
+		return "1 star";
+	}
+	if (Number(movesMade.innerText) > 35) {
+		return "0 stars";
+	}
 }
-
-                
