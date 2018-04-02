2e1681a4ac191b48547d9fd2905127e4791a7589
Suzette Sonnekus <suzetsonekis@gmail.com>
2018-04-02 10:54:23 +0200

    Fixed bug in the timer functionality

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 // global variables
-var clock; 
-var currentcard;
-var secCounter = 0;
+
 let movesMade;
 let star;
 
@@ -12,35 +10,59 @@ let allCardsClosedArray = [...allCardsClosed];
 let openCards = [];
 // create empty array to store which cards are already matched
 let matchedCards = []
-//Create a timer
 
+/*
+ * Timer functionality 
+ */
 
-function startTimer() {
-	clock= setInterval(elapsedTime,1000);
+var h1 = document.getElementsByTagName('h1')[0],
+    start = document.getElementById('start'),
+    stop = document.getElementById('stop'),
+    clear = document.getElementById('clear'),
+    seconds = 0, minutes = 0, hours = 0,
+    t;
+
+function add() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+    
+    h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+
+    timer();
+}
+function timer() {
+    t = setTimeout(add, 1000);
 }
 
+// Stop timer function
 function stopTimer() {
-	clearInterval(clock);
+    clearTimeout(t);
 }
 
-function elapsedTime() {
-	secCounter++;
-	//console.log(secCounter);
-	document.getElementById('seconds').innerText = secCounter;
-}
-
+// Clear timer function
 function resetTimer() {
-	secCounter = 0;
+    h1.textContent = "00:00:00";
+    seconds = 0; minutes = 0; hours = 0;
 }
 
-//mainGameLogic function
+/*
+ * mainGameLogic function 
+ */
+
 
 function mainGameLogic() {
-	if (secCounter > 0) {
+	if (seconds > 0) {
 		console.log('timer has already started');
 	} else {
 		console.log('timer started')
-		startTimer();
+		timer();
 	}
 	console.log(currentcard);
 // True = check if card is valid to click on
@@ -239,29 +261,9 @@ function setUpEventListenerForCards () {
 // this sets up event listeners on the page
 setUpEventListenerForCards();
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-/*function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}*/
 
 
+//shuffle function
 function shuffle(){
 	var ul = document.querySelector('.deck');
 	console.log(ul);
@@ -269,13 +271,12 @@ function shuffle(){
     	ul.appendChild(ul.children[Math.random() * i | 0]);
 	}
 	console.log(ul);
-
 }
-
 
 /* 
  * Restart button
  */
+ 
 // This function removes the classes .show .open and .match all cards only have class .card
 function restartGame () {
 	//display all cards in closed condition
@@ -290,12 +291,12 @@ function restartGame () {
 	console.log('stopped timer');
 	resetTimer();
 	//reset seconds to zero
-	document.querySelector('#seconds').innerText = '00';
+	resetTimer();
 	//reset moves made to zero
 	document.querySelector('.moves').innerText = '0';
 	//reset star rating back to three stars
 	updateStarCounter();
-	shuffle(allCardsClosedArray);
+	shuffle();
 }
 
 //function for modal pop-up
